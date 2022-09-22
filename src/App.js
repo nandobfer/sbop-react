9221b2e6586b01ee2031a317e61677058bfcc6a6
nandobfer <nandobfer@yahoo.com.br>
2022-09-22 03:34:09 -0300

    feat: added profile page router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './pages/Home';
 import './sass/App.scss';
 import './sass/_input.scss';
 import './sass/_button.scss';
+import { Perfil } from './pages/Perfil';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route index element={<Home />} />
+            <Route path='/perfil' element={<Perfil />} />
             {/* <Route path='/cadastrar/' element={<Cadastro />} /> */}
         </Routes>
     </BrowserRouter>
---
+++ src/pages/Perfil/index.jsx
@@ -0,0 +1,8 @@
+import './style.scss';
+
+export const Perfil = () => {
+    
+    return (
+         <h1>teste</h1>
+    )
+}
