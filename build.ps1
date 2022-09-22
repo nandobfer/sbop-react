d8aabd43d9f5e281a179445e170ffe50171535f5
nandobfer <nandobfer@yahoo.com.br>
2022-09-21 22:54:23 -0300

    update: commented api part

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@ yarn build
 Write-Output 'Uploading files to hostgator'
 scp -r -P 22022 build/* root@agenciazop.com.br:/home/sbop/sistema/
 ssh hostgator "chown -R sbop:sbop /home/sbop/sistema/*"
-ssh hostgator "screen -XS bapka-api quit && cd /home/sbop/sistema/bapka-api/ && git pull && sh /scripts/fernando/bapka-api.sh"
+# ssh hostgator "screen -XS sbop-api quit && cd /home/sbop/sistema/sbop-api/ && git pull && sh /scripts/fernando/sbop-api.sh"
