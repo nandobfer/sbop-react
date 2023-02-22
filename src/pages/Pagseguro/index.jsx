1924b84bc107f95a164edbec34dff2c5d0dba5f1
ispadilha <israel.padilha@gmail.com>
2023-02-22 11:55:34 -0300

    testing qrcode size

--- src/pages/Pagseguro/index.jsx
+++ src/pages/Pagseguro/index.jsx
@@ -92,7 +92,7 @@ export const Pagseguro = () => {
                 <p>{qrCode?.id}</p>
                 <p>{qrCode?.text}</p> */}
                 <div className="qr-column">
-                    {qrCode?.text ? <QRCode value={qrCode.text} size={300} /> : null}
+                    {qrCode?.text ? <QRCode value={qrCode.text} size={400} /> : null}
                 </div>
             </div>
         </div>
