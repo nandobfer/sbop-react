544fffd6136da4bd1c9580dfc579dee2cbc330fe
nandobfer <nandobfer@yahoo.com.br>
2023-03-10 14:14:57 -0300

    update: build script

--- iframe_build.ps1
+++ iframe_build.ps1
@@ -1,4 +1,4 @@
 yarn build
 Write-Output 'Uploading build to server'
-scp -r -P 22022 build/* root@agenciazop.com.br:/home/sbop/sistema/
-ssh -p 22022 root@agenciazop.com.br "chown -R sbop:sbop /home/sbop/sistema/*"
+scp -r -P 22022 build/* root@agenciaboz.com.br:/home/sbop/sistema/
+ssh -p 22022 root@agenciaboz.com.br "chown -R sbop:sbop /home/sbop/sistema/*"
