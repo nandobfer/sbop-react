d4025430c0e8b741883bd385bd4df1fecaa5b8d5
nandobfer <nandobfer@yahoo.com.br>
2023-03-20 06:35:18 -0300

    feat: populated content

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -4,7 +4,14 @@ export const Content = ({ content }) => {
     
     return (
         <div className='Content-Component' >
-            <p>{content?.titulo}</p>
+            <h1>{content.titulo}</h1>
+            <p>{content.resumo}</p>
+            <p dangerouslySetInnerHTML={{__html: content.conteudo}}></p>
+            <div className="autor-container">
+                <p>{content.data}</p>
+                <p>{content.autor}</p>
+            </div>
+            <hr />
         </div>
     )
 }
